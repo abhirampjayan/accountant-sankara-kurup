8f0b61d124290ddad4d0fbc4cd470af7d1eb3619
abhirampjayan <abhiramp08@gmail.com>
2024-03-28 10:39:56 +0530

    fonts added

--- App.tsx
+++ App.tsx
@@ -1,36 +1,23 @@
 import Button from 'components/Button';
+import { Image } from 'expo-image';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import colors from 'utils/colors';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.text}>Namaskaram, Sankara Kurup Here</Text>
-        <Button
-          onPress={() => console.log('Button pressed')}
-          title='Press me'
-          variant='primary'
-        />
-        <Button
-          onPress={() => console.log('Button pressed')}
-          title='Press me'
-          variant='secondary'
-        />
-        <Button
-          onPress={() => console.log('Button pressed')}
-          title='Press me'
-          variant='outlined'
-        />
-        <Button
-          onPress={() => console.log('Button pressed')}
-          title='Press me'
-          variant='default'
-        />
-        <StatusBar style='auto' />
+    <>
+      <View>
+        <Image source={require('./assets/landing.png')} style={styles.image} />
       </View>
-    </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Take control of your finance</Text>
+
+          <StatusBar style='auto' />
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
@@ -40,8 +27,16 @@ const styles = StyleSheet.create({
     backgroundColor: colors.light,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 24,
   },
   text: {
     color: colors.dark,
+    fontSize: 36,
+    fontFamily: 'Cormorant Garamond Medium',
   },
+  image: {
+    width: '100%',
+    aspectRatio: 3 / 4,
+  },
+  header: {},
 });
