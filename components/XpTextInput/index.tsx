c6f15b44b34ebd6e9ebce0f40fd0107490b9df33
abhirampjayan <abhiramp08@gmail.com>
2024-03-31 01:52:14 +0530

    Custom TextInput compoent modified

--- components/XpTextInput/index.tsx
+++ components/XpTextInput/index.tsx
@@ -1,12 +1,23 @@
 import React, { ComponentProps } from 'react';
 import { StyleSheet, TextInput } from 'react-native';
+import colors from 'utils/colors';
 
 type Props = ComponentProps<typeof TextInput>;
 
 const XpTextInput = (props: Props) => {
-  return <TextInput {...props} />;
+  return <TextInput style={styles.input} {...props} />;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    width: '100%',
+    borderColor: colors.secondary,
+    borderWidth: 1,
+    minWidth: 300,
+  },
+});
 
 export default XpTextInput;
